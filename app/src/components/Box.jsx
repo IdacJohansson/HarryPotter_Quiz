2029f063c5c30ae42f150b22980b6d2437cebd1d
aycamervetohumcu <amucar06@gmail.com>
2024-12-04 18:23:59 +0100

    imagecomponent.jsx is added, image is added to the box

--- app/src/components/Box.jsx
+++ app/src/components/Box.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import StartButton from "./StartButton";
 
+import Image from "./ImageComponent";
+
 function Box({ title, children }) {
   return (
     <div className="welcome-container">
       {/* put the image component here: <Image />*/}
+
+      <Image src={imageSrc} alt={imageAlt} />
 
       <h1 className="welcome-heading">{title}</h1>
       <br></br>
